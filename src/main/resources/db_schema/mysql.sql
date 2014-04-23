073195349819b01395f6f617657cce1475fd9dd3
Anthony Lo <loanthony09@gmail.com>
2014-04-23 15:42:42 -0500

    Added ojdbc driver, along with mysql and oracle sql files

---
+++ src/main/resources/db_schema/mysql.sql
@@ -0,0 +1,71 @@
+CREATE SCHEMA IF NOT EXISTS mobile_questions;
+
+USE mobile_questions;
+
+DROP TABLE IF EXISTS mobile_questions.dm_user_friends;
+DROP TABLE IF EXISTS mobile_questions.dm_account_active;
+DROP TABLE IF EXISTS mobile_questions.dm_message;
+DROP TABLE IF EXISTS mobile_questions.dm_user;
+DROP TABLE IF EXISTS mobile_questions.dm_contact_info;
+DROP TABLE IF EXISTS mobile_questions.dm_account_jumbo;
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_contact_info (
+	contact_info_id		BIGINT auto_increment,
+	email_address		VARCHAR(50),
+	phone_number		VARCHAR(14),
+	primary key (contact_info_id)
+);
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_account_jumbo (
+	account_id		BIGINT auto_increment,
+	username		VARCHAR(30) not null unique,
+	password		VARCHAR(30) not null,
+	secret_question VARCHAR(75) not null,
+	secret_answer	varchar(50) not null,
+	primary key (account_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_account_active (
+	account_id		BIGINT not null,
+	logged_in		smallint(1) not null default 0,
+	session_id		varchar(20) not null,
+	primary key (account_id),
+	foreign key (account_id) 
+		references dm_account_jumbo(account_id) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_user (
+	user_id			BIGINT auto_increment unique,
+	first_name		VARCHAR(50) not null,
+	last_name   	VARCHAR(50) not null,
+	age				INT not null,
+	contact_info_id	BIGINT,
+	account_id 		BIGINT not null,
+	primary key (user_id),
+	foreign key (contact_info_id) 
+		references dm_contact_info(contact_info_id) on delete set null,
+	foreign key (account_id) 
+		references dm_account_jumbo(account_id) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_user_friends (
+	user_id			BIGINT not null,
+	friend_id		BIGINT not null,
+	primary key (user_id, friend_id),
+	foreign key (user_id)
+		references dm_user(user_id) on delete cascade,
+	foreign key (friend_id) 
+		references dm_user(user_id) on delete cascade
+);
+
+CREATE TABLE IF NOT EXISTS mobile_questions.dm_message (
+	message_id		BIGINT not null,
+	user_id			BIGINT not null,
+	friend_id		BIGINT not null,
+	message			varchar(140) not null,
+	date_posted		timestamp default now(),
+	primary key (message_id, user_id, friend_id),
+	foreign key (user_id) references dm_user(user_id) on delete cascade,
+	foreign key (friend_id) references dm_user(user_id) on delete cascade
+);
---
+++ src/main/resources/db_schema/oracle.sql
@@ -0,0 +1,99 @@
+-- Drop anything related to the Decision Maker application
+DROP TABLE dm_contact_info    CASCADE CONSTRAINTS;
+DROP TABLE dm_account_jumbo   CASCADE CONSTRAINTS;
+DROP TABLE dm_message         CASCADE CONSTRAINTS;
+DROP TABLE dm_user            CASCADE CONSTRAINTS;
+
+DROP SEQUENCE dm_contact_info_seq;
+DROP SEQUENCE dm_account_jumbo_seq;
+DROP SEQUENCE dm_message_seq;
+DROP SEQUENCE dm_user_seq;
+
+-- Tables
+CREATE TABLE dm_contact_info (
+	contact_info_id       number(10) not null,
+	email_address		  varchar(50),
+	phone_number		  varchar(14),
+	primary key (contact_info_id)
+);
+
+CREATE TABLE dm_account_jumbo (
+	account_id          number(10)  not null,
+	username		    varchar(30) not null unique,
+	password		    varchar(30) not null,
+	secret_question     varchar(75) not null,
+	secret_answer	    varchar(50) not null,
+	primary key (account_id)
+);
+
+CREATE TABLE dm_user (
+	user_id             number(10)  not null,
+	first_name		    varchar(50) not null,
+	last_name   	    varchar(50) not null,
+	age				    number(4)   not null,
+	contact_info_id	    number(10),
+	account_id 		    number(10)  not null,
+	primary key (user_id),
+	foreign key (contact_info_id) 
+		references dm_contact_info(contact_info_id) on delete set null,
+	foreign key (account_id) 
+		references dm_account_jumbo(account_id) on delete cascade
+);
+
+CREATE TABLE dm_message (
+	message_id		number(10) not null,
+	user_id			number(10) not null,
+	friend_id		number(10) not null,
+	message			varchar(140) not null,
+	date_posted		date default sysdate,
+	primary key (message_id, user_id, friend_id),
+	foreign key (user_id) references dm_user(user_id) on delete cascade,
+	foreign key (friend_id) references dm_user(user_id) on delete cascade
+);
+
+-- Sequences
+CREATE SEQUENCE dm_contact_info_seq START WITH 1 INCREMENT BY 1 CACHE 100;
+CREATE SEQUENCE dm_account_jumbo_seq START WITH 1 INCREMENT BY 1 CACHE 100;
+CREATE SEQUENCE dm_user_seq START WITH 1 INCREMENT BY 1 CACHE 100;
+CREATE SEQUENCE dm_message_seq START WITH 1 INCREMENT BY 1 CACHE 100;
+
+-- Triggers
+CREATE OR REPLACE TRIGGER dm_contact_info_trigger
+  BEFORE INSERT ON dm_contact_info
+  FOR EACH ROW
+  BEGIN
+    SELECT dm_contact_info_seq.NEXTVAL
+    INTO :new.contact_info_id
+    FROM dual;
+  END;
+/  
+
+CREATE OR REPLACE TRIGGER dm_account_jumbo_trigger
+  BEFORE INSERT ON dm_account_jumbo
+  FOR EACH ROW
+  BEGIN
+    SELECT dm_account_jumbo_seq.NEXTVAL
+    INTO :new.account_id
+    FROM dual;
+  END;
+/
+
+CREATE OR REPLACE TRIGGER dm_user_trigger
+  BEFORE INSERT ON dm_user
+  FOR EACH ROW
+  BEGIN
+    SELECT dm_user_seq.NEXTVAL
+    INTO :new.user_id
+    FROM dual;
+  END;
+/
+
+CREATE OR REPLACE TRIGGER dm_message_trigger
+  BEFORE INSERT ON dm_message
+  FOR EACH ROW
+  BEGIN
+    SELECT dm_message_seq.NEXTVAL
+    INTO :new.message_id
+    FROM dual;
+  END;
+/
